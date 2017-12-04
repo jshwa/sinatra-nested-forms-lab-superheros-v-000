767f82da8b7805c388c15204a82321a338ee5c8d
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 16:45:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../app/") }
+    set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :super_hero.erb
+      erb :super_hero
     end
 
     post '/' do
-      erb :team.erb
+      erb :team
     end
 end
